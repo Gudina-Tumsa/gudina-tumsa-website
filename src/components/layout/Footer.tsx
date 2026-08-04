dad07c4c3f1676e7402cbf3762ff46ee24d03799
abenezer121 <webgoat12@gmail.com>
2026-08-04 11:35:14 +0300

    added footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
+import { Facebook } from "lucide-react";
 
 
 function StoreButtons() {
@@ -132,10 +132,12 @@ const Footer = () => {
                     </div>
                     {/* Social Icons */}
                     <div className="flex space-x-4">
-                        <a href="#" aria-label="Facebook"><Facebook className="text-gray-500 hover:text-gray-900 transition-colors" /></a>
-                        <a href="#" aria-label="Twitter"><Twitter className="text-gray-500 hover:text-gray-900 transition-colors" /></a>
-                        <a href="#" aria-label="Instagram"><Instagram className="text-gray-500 hover:text-gray-900 transition-colors" /></a>
-                        <a href="#" aria-label="LinkedIn"><Linkedin className="text-gray-500 hover:text-gray-900 transition-colors" /></a>
+                        <a href="https://www.facebook.com/share/1BWvzvYur6/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><Facebook className="text-gray-500 hover:text-gray-900 transition-colors" /></a>
+                        <a href="https://www.tiktok.com/@gudinaandtsehaylegacy?_r=1&_t=ZS-98WGzDFkW1s" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="text-gray-500 hover:text-gray-900 transition-colors">
+                                <path d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48Z" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
